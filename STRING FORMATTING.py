fa8ef350292fdaa4f2e81df1929b3bc1e92be908
HRITIK KUMAR SINGH <40272214+hritikpapa@users.noreply.github.com>
2018-10-20 08:33:35 +0530

    Add files via upload

---
+++ STRING FORMATTING.py
@@ -0,0 +1,4 @@
+name,age=input("enter you name and age  ").split()
+print("Hello "+ name + " your age is "+ age)     #booring syntax and too much + sign :( :(
+print("Hello {} youR age is {}".format(name,age))     #wow fantastic and easy to use for multiple variables :) :)
+# WE WILL GET SAME OUTPUT AFTER EXECUTION OF THIS PROGRAM FOR BOTH THE LINES .
